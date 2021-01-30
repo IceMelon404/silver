d96344eac6efa3df19b758b62534c3f4d184c2bd
junsekim <junsekim@student.42seoul.kr>
2021-01-30 18:51:18 +0900

    initial commit

---
+++ standard_function.h
@@ -0,0 +1,10 @@
+#include <unistd.h>
+
+char* t_strcpy(char*dest,char*src);
+char* t_strncpy(char*dest,char*src,unsigned intn);
+char* t_strcapitalize(char*str);
+unsigned int t_strlcpy(char*dest,char*src,unsigned int size);
+void t_hexdump(void *addr, unsigned int size);
+int t_strcmp(char*s1,char*s2);
+int t_strncmp(char*s1,char*s2,unsigned int);
+char* t_strcat(char*dest,char*src);
